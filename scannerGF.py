201533dc2c08c218d134a91c6346429e7ad76b3f
Your Name <you@example.com>
2015-09-16 15:14:00 +0800

    add 360webscan.html

--- scannerGF.py
+++ scannerGF.py
@@ -14,15 +14,21 @@ from utils.pencillog import *
 #log to file
 logging.basicConfig(filename='log.txt',level=logging.DEBUG)
 app = Flask(__name__)
-
+notexisturls = ['neverexist','nevercouldexist']
 #路由所有URL
-@app.route('/', defaults={'path': ''},methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE'])
+
+@app.route('webscan_360_cn.html')
+def webscan():
+    return 'dc5445503925addea4cd3db496a78c66'
+
+
+@app.route('/', defaults={'path': '/'},methods=['GET', 'POST', 'PUT', 'HEAD', 'DELETE'])
 @app.route('/<path:path>',methods=['GET', 'POST', 'PUT', 'HEAD','DELETE'])
 def catch_all(path):
 	Log.d(str(request.method) + ' ' + str(request.url) +' ' + str(request.get_data()))
-
-	if 'nevercouldexist' in path:
-		abort(404)
+	for url in notexisturls:
+		if url in path:
+			abort(404)
 
 
 	return 'hello my baby!'
